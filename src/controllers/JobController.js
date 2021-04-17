5296c1ef3b5100af6b38a2a8f379f3e3c4e8144f
Sara Martins <sariinha.martins@gmail.com>
2021-04-17 00:11:29 -0300

    validating new job name

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -4,18 +4,28 @@ const Profile = require("../model/Profile");
 
 module.exports = {
   create(req, res) {
-    return res.render("job");
+    return res.render("job", { errors: undefined });
   },
 
   async save(req, res) {
-    await Job.create({
-      name: req.body.name,
-      "daily-hours": req.body["daily-hours"],
-      "total-hours": req.body["total-hours"],
-      created_at: Date.now(), // atribuindo data de hoje
-    });
+    let errors = [];
 
-    return res.redirect("/");
+    if (req.body.name == "" || req.body.name == null) {
+      errors.push({ text: "Nome inválido" });
+    }
+
+    if (errors.length > 0) {
+      return res.render("job", { errors });
+    } else {
+      await Job.create({
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+        created_at: Date.now(), // atribuindo data de hoje
+      });
+
+      return res.redirect("/");
+    }
   },
 
   async show(req, res) {
